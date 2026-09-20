df7a6f17fb0b6fe2c5421a643319d8b37f54fa6c
kepler <3827200344@qq.com>
2026-09-20 23:10:57 +0800

    编写spin_base基类（未完成）

--- tasks/motion_generator/spin/spin_base/spin_base.cpp
+++ tasks/motion_generator/spin/spin_base/spin_base.cpp
@@ -6,5 +6,17 @@ SpinBase::SpinBase(const YAML::Node& config)
 {
     number_of_sine_functions_ = config["number_of_sine_functions"] ? config["number_of_sine_functions"].as<int>() : 1;
     
+    // 初始化正弦函数
+    sine_functions_.reserve(number_of_sine_functions_);
+    A_lists_.reserve(number_of_sine_functions_);
+    f_lists_.reserve(number_of_sine_functions_);
+    phi_lists_.reserve(number_of_sine_functions_);
+    x_lists_.reserve(number_of_sine_functions_);
+    
+    for (int i = 0; i < number_of_sine_functions_; i++)
+    {   
+        tools::SineFunction sine_function{A_lists_[i], f_lists_[i], phi_lists_[i], x_lists_[i]};
+        sine_functions_.emplace_back(sine_function);
+    }
 }
 } // namespace motion_generator
--- tasks/motion_generator/spin/spin_base/spin_base.hpp
+++ tasks/motion_generator/spin/spin_base/spin_base.hpp
@@ -14,9 +14,19 @@ class SpinBase
 public:
     SpinBase(const YAML::Node& config);
     virtual ~SpinBase() = default;
-private:
+
+    virtual double get_states() const;
+    
+
+protected:
     int number_of_sine_functions_; // 正弦函数的数量
     std::vector<tools::SineFunction> sine_functions_;
+
+    // 正弦函数运动参数
+    std::vector<double> A_lists_;
+    std::vector<double> f_lists_;
+    std::vector<double> phi_lists_;
+    std::vector<double> x_lists_;
 };
 
 } // namespace motion_generator
