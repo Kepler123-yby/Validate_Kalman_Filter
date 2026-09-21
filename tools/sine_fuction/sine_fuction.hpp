637086fd8ffb6b8cdc6139cf3b2c43484022a1f0
kepler <3827200344@qq.com>
2026-09-21 23:21:38 +0800

    加入积分功能

--- tools/sine_fuction/sine_fuction.hpp
+++ tools/sine_fuction/sine_fuction.hpp
@@ -2,6 +2,7 @@
 #define _SINE_FUNCTION_HPP_
 
 #include <cmath>
+#include <chrono>
 
 namespace tools
 {
@@ -19,6 +20,17 @@ struct SineFunction
     double evaluate(double t) const
     {
         return A * std::sin(f * t + phi) + x;
+    }
+
+    double integral(const std::chrono::steady_clock::time_point& time)
+    {
+        auto t = std::chrono::duration<double>(time.time_since_epoch()).count();
+        return A * f * std::cos(f * t + phi);
+    }
+
+    double get_all_theta(const std::chrono::steady_clock::time_point& time1, const std::chrono::steady_clock::time_point& time2)
+    {
+        return integral(time1) - integral(time2);
     }
 
 };
